6ac07f769c352ac0237ad570b2739cf66d1f1ac3
sushan242833 <sushan.poudel.55555@gmail.com>
2026-06-30 10:36:40 +0545

    fix: minor fix on contact route

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import { submitContactForm } from "@controllers/contact.controller";
 import { validateContactForm } from "@validation/contact.validation";
-import { EmptyRequestParams } from "@app-types/http.requests";
 
 const router = Router();
 
-router.post<EmptyRequestParams, unknown, unknown>(
-  "/",
-  validateContactForm,
-  submitContactForm,
-);
+router.post("/", validateContactForm, submitContactForm);
 
 export default router;
